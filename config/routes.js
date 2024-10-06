4e6d07243c74cdd830ceaa8755c321335fad657c
Rae-An Andres <raeandres@users.noreply.github.com>
2024-10-06 23:58:12 +0800

    Routes changes
    Added routes.js to act as middleware router to the Controller

---
+++ config/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+
+// Controllers
+const pages = new (require('../app/controllers/pages'))()
+
+router.get('/', pages.welcome)
+
+module.exports = router
