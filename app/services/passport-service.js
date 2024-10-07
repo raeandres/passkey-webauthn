093c9d5b5dacfc3daec6efcfbcdcd15b89c6be55
Rae-An Andres <raeandres@users.noreply.github.com>
2024-10-08 00:45:11 +0800

    Passport authn changes
    Created passport-service.js

---
+++ app/services/passport-service.js
@@ -0,0 +1,9 @@
+class PassportService {
+    init(store){
+        // 1. Configure passport to use WebAuthn Strategy
+        // 2. passport serialize error
+        // 3. passport deserialize user
+    }
+}
+
+module.exports = PassportService
